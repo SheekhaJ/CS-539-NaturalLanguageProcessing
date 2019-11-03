4c8b4b0f36509da9ee345bad548c3c218564e3b1
Sheekha Jariwala <jariwals@oregonstate.edu>
2019-11-02 20:15:56 -0700

    Final version of my-estimate.py

--- hw2/hw2-data/my-estimate.py
+++ hw2/hw2-data/my-estimate.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-lines = [l.strip() for l in open('given1.data','r').readlines()]
+lines = [l.strip() for l in open('epron-jpron.data','r').readlines()]
 elines, jlines, alignlines = [], [], []
 
 for i,l in enumerate(lines):
@@ -15,28 +15,44 @@ for i,l in enumerate(lines):
 # print(jlines)
 # print(alignlines)
 
-mapping = defaultdict(list)
+mapping, mappingCount = defaultdict(list), defaultdict(lambda : defaultdict(lambda:float(0)))
 for i,al in enumerate(alignlines):
-    temp = [[None,1] for _ in range(max(map(int,al.split())))]
+    temp = [None for _ in range(max(map(int,al.split())))]
     jchars = jlines[i].split()
     echars = elines[i].split()
     
     for j,a in enumerate(al.split()):
         a = int(a)
-        if temp[a-1][0]!=None and len(temp[a-1][0]+jchars[j]) > 3:
+        if temp[a-1]!=None and len(temp[a-1]+jchars[j]) > 3:
             continue
-        temp[a-1][0] = temp[a-1][0]+jchars[j] if temp[a-1][0]!=None else jchars[j]
+        temp[a-1] = temp[a-1]+jchars[j] if temp[a-1]!=None else jchars[j]
     tempDict = {echars[j]:temp[j] for j in range(len(temp))}
     
-    # print(''.join(echars), mapping, tempDict)
-    for k,v in tempDict.items():
-        # print([t[0] for t in mapping[k]])
-        # print(k, mapping[k], v[0])
-        # if v[0] not in [t[0] for t in mapping[k]]:
-        if v not in mapping[k]:
-            mapping[k].append(v)
-        else:
-            x = mapping[k].index(v)
-            mapping[k][x][1] += 1
+    # print(''.join(echars),tempDict)
 
-print(mapping)
+    for k,v in tempDict.items():
+        mapping[k].append(v)
+
+for echar,jchars in mapping.items():
+    for jchar in jchars:
+        mappingCount[echar][jchar] += 1 
+
+# print(mapping)
+# print(mappingCount)
+
+probs = {echar : {jchar:mappingCount[echar][jchar]/sum(mappingCount[echar].values()) for jchar in jcharcounts} for echar,jcharcounts in mappingCount.items()}
+
+for echar, jcharprobs in probs.items():
+    for jchar, prob in jcharprobs.items():
+        if prob<0.01:
+            probs[echar][jchar] = float(0)
+
+probs = {echar : {jchar:probs[echar][jchar]/sum(probs[echar].values()) for jchar in jcharcounts} for echar,jcharcounts in probs.items()}
+
+outStr = ''
+for echar, jchars in sorted(probs.items()):
+    for jchar,prob in sorted(jchars.items()):
+        if len(jchar.split()) > 0:
+            for c in jchar.split():
+                outStr += echar+' : '+ c +' # '+str(prob)+'\n'
+print(outStr)
