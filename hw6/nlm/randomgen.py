8f97e7d6c6dc605310835ddbc8ee28da8fdf65fb
Sheekha Jariwala <jariwals@oregonstate.edu>
2019-12-09 22:09:34 -0800

    Fixed randomgen to use only chars which are present in keys of h.next_prob()

--- hw6/nlm/randomgen.py
+++ hw6/nlm/randomgen.py
@@ -2,23 +2,25 @@ import sys
 import math
 from nlm import NLM
 from random import choices
+import random
 
 fout = sys.stdout
 
-NLM.load('base')
+NLM.load('large')
 sentences = []
 for _ in range(10):
     h = NLM()
     sent = ''
-    choicechar = choices(population= [chr(a) for a in range(97,123)]+['_','</s>'], weights= h.next_prob().values())[0]
-    i = 0
+    choicekeys = list(h.next_prob().keys())
+    choicechar = choices(population = choicekeys, weights = h.next_prob().values())[0]
     while choicechar != '</s>':
         if choicechar != '_':
             sent += choicechar
         else:
             sent += ' '
         h += choicechar
-        choicechar = choices(population= [chr(a) for a in range(97,123)]+['_','</s>'], weights= h.next_prob().values())[0]
+        choicekeys = list(h.next_prob().keys())
+        choicechar = choices(population = choicekeys, weights = h.next_prob().values())[0]
     sentences.append(sent)
 
 for s in sentences:
